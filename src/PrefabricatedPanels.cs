6bc9c9473fe320c8d07d542fe5bab2b95b25d1a5
Ian Keough <ian@hypar.io>
2020-02-21 10:22:23 -0800

    Use trimming logic taken from Structure.

--- src/PrefabricatedPanels.cs
+++ src/PrefabricatedPanels.cs
@@ -9,6 +9,31 @@ using System.Linq;
 
 namespace PrefabricatedPanels
 {
+	internal class IntersectionComparer : IComparer<Vector3>
+    {
+        private Vector3 _origin;
+        public IntersectionComparer(Vector3 origin)
+        {
+            this._origin = origin;
+        }
+
+        public int Compare(Vector3 x, Vector3 y)
+        {
+            var a = x.DistanceTo(_origin);
+            var b = y.DistanceTo(_origin);
+
+            if(a < b)
+            {
+                return -1;
+            }
+            else if(a > b)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
 	public class WallBoardPanel : GeometricElement
 	{
 		public Profile Profile{get;set;}
@@ -71,8 +96,11 @@ namespace PrefabricatedPanels
 			var studProfile = new StudProfile();
 			var outerStudMaterial = new Material("Stud Frame", Colors.Red);
 			var studMaterial = new Material("Stud", new Color(0.7, 0.7, 0.7, 1.0), 0.8f, 0.8f);
+			var headerProfile = new Profile(Polygon.Rectangle(Units.InchesToMeters(3.625), Units.InchesToMeters(3.625)));
+			var headerTrans = new Transform(new Vector3(0, Units.InchesToMeters(3.625)/2));
+			// headerProfile.Transform(headerTrans);
+
 			var centerLineMaterial = new Material("Center Line", Colors.Gray);
-			// var colorScale = new ColorScale(new List<Color>{Colors.Cyan, Colors.Orange}, 5);
 			var wallBoard = new Material("Wall Board", new Color(0.9, 0.9, 0.9, 0.75), 0.0f, 0.0f);
 
 			foreach(var panel in wallPanels)
@@ -80,6 +108,8 @@ namespace PrefabricatedPanels
 				// Draw the panel profiles.
 				// var mc = new ModelCurve(panel.Transform.OfPolygon(panel.Profile.Perimeter), centerLineMaterial);
 				// elements.Add(mc);
+
+				var wallBoardOffset = panel.Profile.Perimeter.Offset(-tolerance);
 
 				// Offset the panel profile
 				var offset = panel.Profile.Perimeter.Offset(-studDepth/2 - tolerance);
@@ -97,8 +127,10 @@ namespace PrefabricatedPanels
 				// elements.AddRange(panelTransform.ToModelCurves());
 
 				// Draw the panel frame.
+				
 				foreach(var seg in outer.Segments())
 				{
+					var l = seg.Length();
 					var d = seg.Direction();
 					var dot = d.Dot(Vector3.ZAxis);
 					var t = seg.TransformAt(0);
@@ -107,7 +139,11 @@ namespace PrefabricatedPanels
 					// elements.AddRange(t.ToModelCurves());
 
 					var beamRotation = t.XAxis.AngleTo(panelPlane.Normal);
-					
+					if(Double.IsNaN(beamRotation))
+					{
+						Console.WriteLine($"l: {l}, d:{d}, dot:{dot}");
+					}
+
 					Vector3 a, b;
 					if(Math.Abs(dot) == 1)
 					{
@@ -120,34 +156,37 @@ namespace PrefabricatedPanels
 						b = seg.End += d * studDepth/2;
 					}
 
-					var beam = new Beam(new Line(a, b), studProfile, outerStudMaterial, rotation: beamRotation);
+					var cl = new Line(a, b);	
+					var beam = new Beam(cl, (dot == 0.0 && seg.Start.Z > 0.1) ? headerProfile : studProfile, outerStudMaterial, rotation: (Double.IsNaN(beamRotation) ? 0.0 : beamRotation));
 					elements.Add(beam);
 				}
 
 				// Draw the panel framing.
+				
 				var grid = new Grid2d(offset[0]);
-				grid.U.DivideByApproximateLength(input.StudSpacing);
+				grid.U.DivideByFixedLength(input.StudSpacing);
 
 				var studCls = grid.GetCellSeparators(GridDirection.V);
 
 				// Only take the inner studs.
-				studCls = studCls.Skip(1).Take(studCls.Count - 2).ToList();
-				foreach(var sep in studCls)
+				var innerCls = studCls.Skip(1).Take(studCls.Count - 2).ToList().Cast<Line>().ToList();
+				var trimmedInnerCls = TrimLinesToBoundary(innerCls, offset[0].Segments());
+
+				foreach(var trimmedLine in trimmedInnerCls)
 				{
-					var cl = (Line)sep;
-					var t = cl.TransformAt(0);
+					var t = trimmedLine.TransformAt(0);
 					var beamRotation = t.XAxis.AngleTo(panelPlane.Normal);
-					var innerBeam = new Beam(panel.Transform.OfLine(cl), studProfile, studMaterial, rotation: beamRotation);
+					var innerBeam = new Beam(panel.Transform.OfLine(trimmedLine), studProfile, studMaterial, rotation: beamRotation);
 					elements.Add(innerBeam);
 				}
 
 				// Create the wall board panels
-				var wallBoardGrid = new Grid2d(offset[0]);
-				wallBoardGrid.U.DivideByApproximateLength(Units.FeetToMeters(8.0));
-				wallBoardGrid.V.DivideByApproximateLength(Units.FeetToMeters(4.0));
+				var wallBoardGrid = new Grid2d(wallBoardOffset[0]);
+				wallBoardGrid.U.DivideByFixedLength(Units.FeetToMeters(8.0));
+				wallBoardGrid.V.DivideByFixedLength(Units.FeetToMeters(4.0));
 				foreach(var cell in wallBoardGrid.CellsFlat)
 				{
-					var panelPerimeter = (Polygon)cell.GetCellGeometry();
+					var panelPerimeter = (Polygon)cell.GetTrimmedCellGeometry()[0];
 					var leftTrans = new Transform(panel.Transform);
 					leftTrans.Move(leftTrans.ZAxis * studWidth/2);
 					var panelL = new WallBoardPanel(panelPerimeter, leftTrans, wallBoard);
@@ -165,6 +204,96 @@ namespace PrefabricatedPanels
 			var output = new PrefabricatedPanelsOutputs(panelCount);
 			output.model.AddElements(elements);
             return output;
+        }
+
+		private static List<Line> TrimLinesToBoundary(List<Line> lines, IList<Line> boundarySegements)
+        {
+            var trims = new List<Line>();
+            foreach(var grid in lines)
+            {
+                var xsects = new List<Vector3>();
+                foreach(var s in boundarySegements)
+                {
+                    if(!Intersects(s, grid, out Vector3 xsect))
+                    {
+                        continue;
+                    }
+                    xsects.Add(xsect);
+                }
+
+                if(xsects.Count < 2)
+                {
+                    continue;
+                }
+
+                xsects.Sort(new IntersectionComparer(grid.Start));
+
+                for(var i=0; i<xsects.Count-1; i+=2)
+                {
+                    if(xsects[i].IsAlmostEqualTo(xsects[i+1]))
+                    {
+                        continue;
+                    }
+                    trims.Add(new Line(xsects[i], xsects[i+1]));
+                }
+            }
+            return trims;
+        }
+
+        /// <summary>
+        /// https://social.msdn.microsoft.com/Forums/vstudio/en-US/e5993847-c7a9-46ec-8edc-bfb86bd689e3/help-on-line-segment-intersection-algorithm?forum=csharpgeneral
+        /// </summary>
+        /// <param name="AB"></param>
+        /// <param name="CD"></param>
+        /// <returns></returns>
+        public static bool Intersects(Line AB, Line CD, out Vector3 result) {
+            double deltaACy = AB.Start.Y - CD.Start.Y;
+            double deltaDCx = CD.End.X - CD.Start.X;
+            double deltaACx = AB.Start.X - CD.Start.X;
+            double deltaDCy = CD.End.Y - CD.Start.Y;
+            double deltaBAx = AB.End.X - AB.Start.X;
+            double deltaBAy = AB.End.Y - AB.Start.Y;
+
+            double denominator = deltaBAx * deltaDCy - deltaBAy * deltaDCx;
+            double numerator = deltaACy * deltaDCx - deltaACx * deltaDCy;
+
+            result = new Vector3();
+
+            if (denominator == 0) 
+            {
+                if (numerator == 0) {
+                    // collinear. Potentially infinite intersection points.
+                    // Check and return one of them.
+                    if (AB.Start.X >= CD.Start.X && AB.Start.X <= CD.End.X) {
+                        result = AB.Start;
+                        return true;
+                    } else if (CD.Start.X >= AB.Start.X && CD.Start.X <= AB.End.X) {
+                        result = CD.Start;
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } 
+                else 
+                { // parallel
+                    return false;
+                }
+            }
+
+            double r = numerator / denominator;
+            if (r < 0 || r > 1) 
+            {
+                return false;
+            }
+
+            double s = (deltaACy * deltaBAx - deltaACx * deltaBAy) / denominator;
+            if (s < 0 || s > 1) 
+            {
+                return false;
+            }
+
+            result = new Vector3 ((AB.Start.X + r * deltaBAx), (AB.Start.Y + r * deltaBAy));
+            return true;
         }
     }
 }
